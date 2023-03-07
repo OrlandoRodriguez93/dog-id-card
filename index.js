5cc3038923fc23ef8231ed350420399203860284
orlando.rodriguez93 <orlando.rodriguez93@hotmail.com>
2023-03-06 21:34:30 -0600

    /laura path

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 app.use(express.static(path.join(__dirname, 'app')));
  
-router.get('/',function(req,res){
+router.get('/laura',function(req,res){
   res.sendFile(path.join(__dirname+'/app/lauras-id.html'));
 });
 
